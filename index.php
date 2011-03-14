5aa83f1f59b78e000f1431a9ff19b66ec99f1dec
tylerflint <tylerflint@gmail.com>
2011-03-14 16:51:52 -0600

    adding boxfile, testing connection

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+
 <?php
 
-echo "this is going to be a simple test";
+/*** mysql hostname ***/
+$hostname = 'localhost:/tmp/mysql/diana.sock';
+
+/*** mysql username ***/
+$username = 'jillian';
+
+/*** mysql password ***/
+$password = 'GQJEy0ir';
+
+try {
+    $dbh = new PDO("mysql:host=$hostname;dbname=mysql", $username, $password);
+    /*** echo a message saying we have connected ***/
+    echo 'Connected to database';
+    }
+catch(PDOException $e)
+    {
+    echo $e->getMessage();
+    }
+?>
